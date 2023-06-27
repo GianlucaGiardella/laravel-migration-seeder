8ff44eb103bdc48ed9227d796fbd94867079a9ab
GianluGiarde <gianlugiarde@gmail.com>
2023-06-27 17:30:51 +0200

    change title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/js/app.js')
-    <title>Template</title>
+    <title>Laravel Migration Seeder</title>
 </head>
 <body>
-    <h1>Template di base</h1>
 </body>
 </html>
