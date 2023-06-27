335fde4942b95717007319b0418c2fbaf9feab38
GianluGiarde <gianlugiarde@gmail.com>
2023-06-28 01:16:54 +0200

    add bootstrap table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,31 @@
     @vite('resources/js/app.js')
     <title>Laravel Migration Seeder</title>
 </head>
-<body>
-    <h1>Laravel Trains</h1>
-
-    @foreach ($trains as $train)
-        <li>
-            {{$train->brand}} {{$train->train_code}} - Partenza: {{$train->departure_station}} alle {{$train->departure_time}} - Arrivo: {{$train->arrival_station}} alle {{$train->arrival_time}}
-        </li>
-    @endforeach
+<body data-bs-theme="dark">
+    <h1 class="mb-4">Laravel Trains</h1>
+    <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Treno</th>
+            <th scope="col">Numero</th>
+            <th scope="col">Cabine</th>
+            <th scope="col">Partenza</th>
+            <th scope="col">Arrivo</th>
+            <th scope="col">Ritardo</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <td>{{$train->brand}}</td>
+                    <td>{{$train->train_code}}</td>
+                    <td>{{$train->carriages}}</td>
+                    <td>{{$train->departure_station}} alle {{$train->departure_time}}</td>
+                    <td>{{$train->arrival_station}} alle {{$train->arrival_time}}</td>
+                    <td>{{$train->in_time ? "Si" : "No"}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+      </table>
 </body>
 </html>
